a2f28c31fc3b2f18034c8a0d7225928be31bcafd
你的使用者名稱 <C111181128@nkust.edu.tw>
2025-06-04 19:44:36 +0800

    v1.04

--- app.py
+++ app.py
@@ -58,4 +58,5 @@ def predict():
         os.remove(path)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # 預設為 5000，但 Render 會給 PORT
+    app.run(host="0.0.0.0", port=port)
